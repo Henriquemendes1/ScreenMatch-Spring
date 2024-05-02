3e0a6b1287c763862be5abd13798fe6942f3d5aa
Henriquemendes1 <henrique.mend258@gmail.com>
2024-05-02 14:51:02 -0300

    Commit

--- src/main/java/br/com/alura/screenMartch/ScreenMartchApplication.java
+++ src/main/java/br/com/alura/screenMartch/ScreenMartchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenMartch;
 
-import br.com.alura.screenMartch.models.DadosSerie;
-import br.com.alura.screenMartch.service.ConsumoApi;
-import br.com.alura.screenMartch.service.ConverteDados;
+import br.com.alura.screenMartch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +14,10 @@ public class ScreenMartchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Gilmore+Girls&apikey=64a6d7f7&");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
 	}
+
+
 }
